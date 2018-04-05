bbd98b727ff0db3285d8dc1460fb8d69d09369fb
Veselin Tonev <veselinn16@gmail.com>
2018-04-05 11:24:13 +0300

    Add star removing function

--- js/designs.js
+++ js/designs.js
@@ -1,24 +1,28 @@
-// const clicks = document.querySelector(".information__data__moves");
-
-// let moves = clicks.textContent;
-
-// const card = document.querySelectorAll('.card');
-
-
-// // card.addEventListener('click', function() {
-// //     clicks.textContent = '1';
-// // });
-
-// console.log(card);
 function main() {
-    const $moves = $('.information__data__moves'); //selects moves
-
+    // click counter function
+    const $moves = $(".information__data__moves");
+    const $star1 = $("i").first();
+    const $star2 = $star1.next();
+    const $star3 = $star2.next();
+  
     let $num = 0;
-
-    $('.card').on('click', function() {
+  
+    $(".card").click(function() {
         $num += 1;
-        $moves.text(num);
+        $moves.text($num);
+        // insert smooth transition
+        switch ($num) {
+            case 4:
+                $star1.remove();
+            break;
+            case 8:
+                $star2.remove();
+            break;
+            case 12:
+                $star3.remove();        
+        };
     });
+    
 };
 
 $(document).ready(main);
